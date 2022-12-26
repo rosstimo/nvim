78f05029eead89e656735cc241ef5970e6b5ef09
rosstimo <rosstimo@isu.edu>
2022-12-25 21:40:10 -0700

    add selection movement

--- lua/keymaps/init.lua
+++ lua/keymaps/init.lua
@@ -6,14 +6,18 @@ vim.g.mapleader = ' ' -- leader key to space
 map("n", "<leader>t", ":NvimTreeToggle<CR>", opts)
 map("n", "<leader>gt", ":NvimTreeFocus<CR>", opts)
 
+--move selected line(s) 
+map("v", "J", ":m '>+1<CR>gv=gv", opts)
+map("v", "K", ":m '<-2<CR>gv=gv", opts)
+map("v", "L", ">gv", opts)
+map("v", "H", "<gv", opts)
 
 -- Clipboard Copy/Paste
-
 -- use CTRL-c to copy in visual mode
 map('v', '<C-c>' , '"*y :let @+=@* <CR>', opts)
 
 -- use CTRL-v to paste in normal mode
-map('n', '<C-v>', '"+P', opts) 
+map('n', '<C-v>', '"+P', opts)
 
 -- Use xclip to handle interface "+ and "* registers with system clipboard
 -- xclip must be installed and $DISPLAY must be set
