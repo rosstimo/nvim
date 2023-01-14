2dfc52be0a19280583472da0d6ee5a43c769ce38
rosstimo <rosstimo@isu.edu>
2023-01-14 14:03:50 -0700

    add completion features

--- lua/plugin/lsp/lspkind.lua
+++ lua/plugin/lsp/lspkind.lua
@@ -3,19 +3,60 @@ local lspkind = require('lspkind')
 cmp.setup {
   formatting = {
     format = lspkind.cmp_format({
-      mode = 'symbol', -- show only symbol annotations
+    -- defines how annotations are shown
+    -- default: symbol
+    -- options: 'text', 'text_symbol', 'symbol_text', 'symbol'
+      mode = 'symbol_text',
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
-      with_text = true,
-      menu = {
-        buffer = "[buf]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[api]",
-        path = "[path]",
-        luasnip = "[snip]",
-        --gh_issues = "[issues]",
-        --tn = "[TabNine]",
-      },
+
+    -- default symbol map
+    -- can be either 'default' (requires nerd-fonts font) or
+    -- 'codicons' for codicon preset (requires vscode-codicons font)
+    -- default: 'default'
+    preset = 'codicons',
+    -- override preset symbols
+    -- default: {}
+    symbol_map = {
+      Text = "",
+      Method = "",
+      Function = "",
+      Constructor = "",
+      Field = "ﰠ",
+      Variable = "",
+      Class = "ﴯ",
+      Interface = "",
+      Module = "",
+      Property = "ﰠ",
+      Unit = "塞",
+      Value = "",
+      Enum = "",
+      Keyword = "",
+      Snippet = "",
+      Color = "",
+      File = "",
+      Reference = "",
+      Folder = "",
+      EnumMember = "",
+      Constant = "",
+      Struct = "פּ",
+      Event = "",
+      Operator = "",
+      TypeParameter = ""
+    },
+    menu = {
+      buffer = "[buf]",
+      look = "[look]",
+      nvim_lsp = "[LSP]",
+      nvim_lua = "[api]",
+      path = "[path]",
+      luasnip = "[snip]",
+      --gh_issues = "[issues]",
+      --tn = "[TabNine]",
+    },
+    view = {
+      entries = {name = 'custom', selection_order = 'near_cursor' }
+    },
             -- The function below will be called before any actual modifications from lspkind
       -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
       before = function (entry, vim_item)
@@ -24,4 +65,5 @@ cmp.setup {
       end
     })
   }
+
 }
--- lua/plugin/packerConfig.lua
+++ lua/plugin/packerConfig.lua
@@ -29,9 +29,14 @@ use { -- LSP Configuration & Plugins
     'hrsh7th/nvim-cmp',
     requires = {
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
+      'octaltree/cmp-look',
       'L3MON4D3/LuaSnip',
       'saadparwaiz1/cmp_luasnip',
-      'onsails/lspkind-nvim' --> vscode-like pictograms for neovim lsp completion items
+      'onsails/lspkind-nvim', --> vscode-like pictograms for neovim lsp completion items
+      'neovim/nvim-lspconfig',
     },
   }
 
