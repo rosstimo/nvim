fbaa5032a187bccf1a792c2c70437305f0fa0e5b
rosstimo <rosstimo@isu.edu>
2023-01-14 23:25:15 -0700

    add organization plan

--- init.lua
+++ init.lua
@@ -1,24 +1,28 @@
 require("gui") -- statusline, tabs, notify, theme, etc.
 require("settings")
 require("keymaps")
+-- plugin manager
 require("plugin.packerConfig")
+-- gui
 --require("plugin.notifyConfig")
 --require("plugin.barbarConfig")
 require("keymaps.barbar")
 --require("plugin.luaLineConfig")
-require("plugin.nvimTreeConfig")
+--require("plugin.indentBlankLineConfig")
+--require("plugin.oneDarkConfig")
+--require("plugin.undoTreeConfig")
+--require("plugin.nvimTreeConfig")
+require("plugin.commentConfig")
+-- lsp
 require("plugin.lsp")
 require("plugin.lsp.nvim-cmp")
 require("plugin.lsp.lspkind")
+-- lint
 require("plugin.treeSitterConfig")
 require("plugin.tsRainbowConfig")
-require("plugin.telescopeConfig")
-require("plugin.gitSignsConfig")
---require("plugin.undoTreeConfig")
 require("plugin.colorizerConfig")
-require("plugin.commentConfig")
---require("plugin.indentBlankLineConfig")
---require("plugin.oneDarkConfig")
---require("lsp")
--- print("hello,sucka!!")
+require("plugin.gitSignsConfig")
+-- fzf
+require("plugin.telescopeConfig")
+
 
